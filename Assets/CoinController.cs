f004ad8a7f87a098e75bff9a183e3ac50651cef8
yuuki.takahashi <arikitako21@gmail.com>
2020-09-05 21:52:28 +0900

    Lesson6 課題
    発展課題を修正しました。

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    private GameObject camera;
+
+    private float cameraPosz;
+
     // Start is called before the first frame update
     void Start()
     {
           //回転を開始する角度を設定
 　　　　　　this.transform.Rotate (0,Random.Range (0,360),0);
+
+         this.camera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
+         this.cameraPosz = camera.transform.position.z;
+        
+
           //回転
 　　　　　　this.transform.Rotate (0,3,0);
+
+         if (this.transform.position.z < cameraPosz )
+        {
+            Destroy(this.gameObject);
+        }
     }
-   void OnBecameInvisible(){
-        Destroy (this.gameObject);
-    }
+   
 }
--- Assets/ObjectDestroy.cs
+++ Assets/ObjectDestroy.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class ObjectDestroy : MonoBehaviour
 {
+
+    private GameObject camera;
+
+    private float cameraPosz;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.camera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cameraPosz = camera.transform.position.z;
+
+        if (this.transform.position.z<cameraPosz)
+        {
+            Destroy(this.gameObject);
+        }  
     }
     
-    void OnBecameInvisible(){
-        Destroy (this.gameObject);
-    }　　
+   
 }
